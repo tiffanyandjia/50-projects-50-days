77b5f9aa75e4e9d333c724f91f8c3aec9342c444
tiffanydandjia <tlin37@buffalo.edu>
2024-01-02 19:31:35 -0500

    day2

--- day2/script.js
+++ day2/script.js
@@ -7,6 +7,41 @@ let currentActive = 1;
 
 next.addEventListener("click", () => {
   currentActive++;
-
-  console.log(currentActive);
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
 });
+
+prev.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
+
+function update() {
+  circles.forEachh((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentActive === 1) {
+    prev.disable = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
