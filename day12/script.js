b0dc23225e9c0b97c43e94fc74b94a545780dead
tiffanydandjia <tlin37@buffalo.edu>
2024-01-09 11:55:09 -0500

    random-choice-picker

--- day12/script.js
+++ day12/script.js
@@ -1,10 +1,18 @@
 const tagsEl = document.getElementById("tags");
-const textarea = document.getElementById("texttarea");
+const textarea = document.getElementById("textarea");
 
 textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
 });
 
 function createTags(input) {
@@ -21,4 +29,43 @@ function createTags(input) {
     tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
   });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    if (randomTag !== undefined) {
+      highlightTag(randomTag);
+
+      setTimeout(() => {
+        unHighlightTag(randomTag);
+      }, 100);
+    }
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
 }
