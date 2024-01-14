e38e78f1c0e86ce497b0e6865d566f21982057ab
tiffanydandjia <tlin37@buffalo.edu>
2024-01-13 21:12:37 -0500

    background-slider

---
+++ day15/script.js
@@ -0,0 +1,4 @@
+const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = doucument.getElementById("percentage");
+const remained = document.getElementById("remained");
