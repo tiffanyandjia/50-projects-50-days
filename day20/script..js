99cd2343b0be27fc7c212df75d6ce1bd620124a6
tiffanydandjia <tlin37@buffalo.edu>
2024-01-16 23:50:23 -0500

    drag-n-drop

---
+++ day20/script..js
@@ -0,0 +1,38 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+fill.addEventListener("dragstart", dragsStart);
+fill.addEventListener("dragend", drageEnd);
+
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+}
+
+function dragStart() {
+  this.className += "hold";
+  setTimeout(() => (this.className = "invisible"), 0);
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hovered";
+}
+
+function dragLeave() {
+  this.className = "empty";
+}
+
+function dragDrop() {
+  this.className = "empty";
+  this.append(fill);
+}
