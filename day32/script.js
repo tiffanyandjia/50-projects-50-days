86d71b965e7c0133c33010f4b8f47f1dd0145622
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-02-23 23:10:12 -0500

    notes-app

---
+++ day32/script.js
@@ -0,0 +1,13 @@
+const addBtn = document.getElementById('add')
+
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if(notes) {
+    notes.forEach(note => addNewNote(note))
+}
+
+addBtn.addEventListener('click', () => addNewNote())
+
+function addNewNote(text = '') {
+    const note = document
+}
