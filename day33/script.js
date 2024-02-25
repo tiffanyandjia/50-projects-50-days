ee85f4c4e740621b0f6f9327f4fcc6a856289345
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-02-25 15:57:07 -0500

    animated-countdown

--- day33/script.js
+++ day33/script.js
@@ -20,6 +20,21 @@ function runAnimation() {
     nums.forEach((num, idx) => {
         const nextToLast = nums.length -1
 
-        
+       num.addEventListener('animationed', (e) => {
+        if (e.animationName === 'goIn' && idx !== nextToLast) {
+            num.classList.remove('in')
+            num.classList.add('out')
+        } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+            num.nextElementSibling.classList.add('in')
+        } else {
+            counter.classList.add('hide')
+            finalMessage.classList.add('show')
+        }
+       }) 
     })
 }
+
+replay.addEventListener('click', () => {
+    resetDOM()
+    runAnimation()
+})
