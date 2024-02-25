2e34bfb191cb4712e8eecea40aab300b95fac869
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-02-25 12:30:12 -0500

    animated-countedown

---
+++ day33/script.js
@@ -0,0 +1,25 @@
+const nums = document.querySelectorAll('.nums span')
+const counter = document.querySelector('.counter')
+const finalMessage = document.querySelector('.final')
+const replay = document.querySelector('#replay')
+
+runAnimation()
+
+function resetDOM() {
+    counter.classList.remove('hide')
+    finalMessage.classList.remove('show')
+
+    nums.forEach((num) => {
+        num.classList.value = ''
+    })
+
+    nums[0].classList.add('in')
+}
+
+function runAnimation() {
+    nums.forEach((num, idx) => {
+        const nextToLast = nums.length -1
+
+        
+    })
+}
