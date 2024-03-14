00904f06db6c72d5a035dbc95cfdcaba2148d7c6
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-03-13 23:17:12 -0400

    testimonial box switcher

---
+++ day46/script.js
@@ -0,0 +1,5 @@
+const testimonialsContainer = document.querySelector(".testimonials-container");
+const testimonials = document.querySelector(".testimonial");
+const userImage = document.querySelector(".use-image");
+const username = document.querySelector(".username");
+const role = document.querySelector(".role");
