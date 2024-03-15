ab3509fb83e2303df51d7b5e4e6d53e91096fa05
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-03-14 22:53:37 -0400

    testimonial-box-switcher

--- day46/script.js
+++ day46/script.js
@@ -1,6 +1,6 @@
 const testimonialsContainer = document.querySelector(".testimonials-container");
 const testimonial = document.querySelector(".testimonial");
-const userImage = document.querySelector(".use-image");
+const userImage = document.querySelector(".user-image");
 const username = document.querySelector(".username");
 const role = document.querySelector(".role");
 
@@ -60,4 +60,12 @@ function updateTestimonial() {
   userImage.src = photo;
   username.innerHTML = name;
   role.innerHTML = position;
+
+  idx++;
+
+  if (idx > testimonials.length - 1) {
+    idx = 0;
+  }
 }
+
+setInterval(updateTestimonial, 10000);
